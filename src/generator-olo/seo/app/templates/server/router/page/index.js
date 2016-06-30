a285fafae5c3625017cf1f4db10db7ec9f0996dc
曾艳玲 <939184663@qq.com>
2016-06-30 16:13:27 +0800

    添加config图片

--- src/generator-olo/seo/app/templates/server/router/page/index.js
+++ src/generator-olo/seo/app/templates/server/router/page/index.js
@@ -1,14 +1,15 @@
 /**
 *@module page
 *页面路由模块示例
-*@index - 接口名称
-*index - 接口名称
+*
+*路由名称: ［中间件（可选）,函数］,
+*路由名称: 函数
 */
 
 // var midUser = require('../middleware/user.js');
 
 module.exports = {
-	// index: [midUser.hasLogin,function(req,res){
+	// index1: [midUser.hasLogin,function(req,res){
 	// 	res.render('index',{username: req.session.username});
 	// }]
 	index : function(req,res){
