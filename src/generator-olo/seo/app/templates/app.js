80797134784c738addb63f3e438767815cf850a6
曾艳玲 <939184663@qq.com>
2016-07-07 16:36:50 +0800

    session 初始化

--- src/generator-olo/seo/app/templates/app.js
+++ src/generator-olo/seo/app/templates/app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
     secret: 'biketo',
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: false
 }));
 
 router(app);
