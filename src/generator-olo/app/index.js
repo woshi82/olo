ebb282ad66f57e2d23b4f755285f7fe6ae2ea73e
zengyanling <939184663@qq.com>
2016-12-28 13:42:32 +0800

    feat: 增加动态加载模版的功能

--- src/generator-olo/app/index.js
+++ src/generator-olo/app/index.js
@@ -2,35 +2,76 @@
 'use strict';
 
 var generators = require('yeoman-generator');
+var remote = require('yeoman-remote');
+var rimraf = require('rimraf');
 var chalk = require('chalk');
 var _ = require('lodash');
-
-
+var path =  require('path');
+var fs =  require('fs');
+// var util = require('util');
+var generatorRoot = path.join( __dirname, '../');
 
 module.exports = generators.Base.extend({
     constructor: function() {
         generators.Base.apply(this, arguments);
+        // this.argument('viewName', { type: String, required: true });
+        this.argument('cmdType', { type: String, required: true });
+        console.log(arguments[0]);
+        this.spawnArgument = arguments[0].slice(1);
+    },
+    prepare: function(){
+        if (this.cmdType === 'app') {
+            this._initPrompt();
+        }else {
+            this._otherOperate();
+        }
+    },
+    _otherOperate: function(){
+        try {
+            var PACKAGE_JSON_PATH = './package.json';
+            var packageContent = this.fs.readJSON(PACKAGE_JSON_PATH);
+            this.actName = packageContent.name;
+            this.oloType = packageContent.type;
+        } catch (e) {
+            this.log(
+                '\n' +
+                chalk.red('Tips ') +
+                '请初始化项目:  `' + 
+                chalk.blue('olo init')
+            );
+            return;
+        };
+
+        this._getTemplate(function (err, template) {
+            if (err) return done(err);
+            this.composeWith('olo:'+this.oloType, {
+                options: {
+                    oloType: this.oloType,
+                    actName: this.actName
+                },
+                arguments: this.spawnArgument
+            }, {
+                local: require.resolve('../'+this.oloType+'/'+ this.cmdType)
+            });
+        });
+
+        
 
     },
-
-    // 询问 活动名称
-    promptActName: function() {
+    _initPrompt: function(){
         var done = this.async();
         var defaultName = _.kebabCase(this.appname); // Default to current folder name
         var defaultType = '';
 
         // 读取 package.json 设置默认值
         try {
-            var PACKAGE_JSON_PATH = './package.json';
-            var packageContent = this.fs.readJSON(PACKAGE_JSON_PATH);
+            var packageContent = this.fs.readJSON('./package.json');
             defaultName = packageContent.name;
-            defaultType = packageContent.olotype;
-            // defaultGitHost = packageContent.biketo.git;
+            defaultType = packageContent.type;
         } catch (e) {
             // console.log(e);
         }
-        
-        var done = this.async();    
+ 
         this.prompt([{
             type: 'input',
             name: 'name',
@@ -50,19 +91,54 @@ module.exports = generators.Base.extend({
                 name: 'vue',
                 value: 'vue'
             }]
-        }]).then(function(answers) {
-            
+        }]).then(function(answers) { 
             this.actName = answers.name;
             this.oloType = answers.type;
-            this.composeWith("olo:"+this.oloType, {
-                options: {
-                    oloType: this.oloType,
-                    actName: this.actName
-                }
-            }, {
-                local: require.resolve("../"+this.oloType+"/app")
+
+            this._getTemplate(function (err, template) {
+                if (err) return done(err);
+                this.composeWith("olo:"+this.oloType, {
+                    options: {
+                        oloType: this.oloType,
+                        actName: this.actName
+                    }
+                }, {
+                    local: require.resolve("../"+this.oloType+"/app")
+                });
+                done();
             });
-            done();
         }.bind(this));
+    },
+    _getTemplate: function(callback){
+        var _this = this;
+        var cacheTemplate = path.resolve(generatorRoot, this.oloType),
+            archive = 'https://codeload.github.com/woshi82/olo-template-' + this.oloType + '/tar.gz/master';
+
+        callback = callback || function () {};
+        if (this.options.cleanCache) {
+            this.log(
+                '\n' +
+                chalk.yellow('[getTemplate] clean cache template')
+            );
+            rimraf.sync(cacheTemplate);
+        }
+
+        if (fs.existsSync(cacheTemplate)) {
+            this.log(
+                '\n' +
+                chalk.yellow('[getTemplate] find cache template')
+            );
+            return callback.call(this, null, cacheTemplate);
+        }
+
+        this.log(
+            '\n' +
+            chalk.yellow('[getTemplate] get template from '+ archive)
+        );
+        
+        remote.extract(archive, cacheTemplate,{strip: 1}, function (err) {
+            callback.call(_this, err, cacheTemplate);
+        });
+
     }
 });
--- src/generators.js
+++ src/generators.js
@@ -5,11 +5,22 @@ var path = require('path');
 module.exports = function (cmd) {
     var yeoman = require('yeoman-environment');
     var env = yeoman.createEnv(),
-    	oloType = '';
-    if (cmd != 'app' && cmd != 'frame') {
-    	oloType = require(path.join(process.cwd(),'package.json')).type || 'seo';
-    }
-    var gPath = './' + path.join('generator-olo',oloType, cmd, 'index.js');
-    env.register(require.resolve(gPath), cmd);
-    env.run(Array.prototype.join.call(arguments, ' '));
+    	argumentsArray = Array.prototype.slice.call(arguments,0),
+    	cleanCacheIndex = argumentsArray.indexOf('clean');
+    	
+    if (cmd != 'frame') {
+	    if (cleanCacheIndex !== -1) {
+	    	argumentsArray.splice(cleanCacheIndex, 1);
+	    	console.log(argumentsArray)
+	    }
+	    var gPath = './' + path.join('generator-olo', 'app', 'index.js');
+	    env.register(require.resolve(gPath), 'app');
+	    env.run('app ' + argumentsArray.join(' '), {
+	    	cleanCache:  (cleanCacheIndex !== -1)
+	    });
+	}else {
+		var gPath = './' + path.join('generator-olo', cmd, 'index.js');
+	    env.register(require.resolve(gPath), cmd);
+	    env.run(Array.prototype.join.call(arguments, ' '));
+	}
 };
